542f0c8075e3e26b46c71e877006ffb412a491a3
Bhavish Khanna Narayanan <bhavish96.n@gmail.com>
2019-03-30 16:58:35 -0500

    Update local_settings.py

--- expense_track/local_settings.py
+++ expense_track/local_settings.py
@@ -3,12 +3,12 @@ import pyrebase
 
 # Firebase settings
 config = {    
-    'apiKey': "AIzaSyCO8YUAAt30zXH-STsURBlS4SvGEKhfFdo",
-    'authDomain': "expense-track-io.firebaseapp.com",
-    'databaseURL': "https://expense-track-io.firebaseio.com",
-    'projectId': "expense-track-io",
-    'storageBucket': "expense-track-io.appspot.com",
-    'messagingSenderId': "324272958377" 
+    'apiKey': "",
+    'authDomain': "",
+    'databaseURL': "",
+    'projectId': "",
+    'storageBucket': "",
+    'messagingSenderId': "" 
 }
 
 FIREBASE_APP_INIT = pyrebase.initialize_app(config)
